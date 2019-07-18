d5c0ac102da304052f014165c0ac61988bafe455
SincereCSL <paranoid88@126.com>
2019-07-18 18:24:11 +0800

    node.js 文件内容截取 同步方法实现

--- NodeProgect/fileSystemOperate_fs_truncate.js
+++ NodeProgect/fileSystemOperate_fs_truncate.js
@@ -1,7 +1,16 @@
 //node.js 文件内容截取
 
+// var fs = require('fs');
+// fs.truncate('./fileForRead.txt',6,function(err){
+//    if (err) throw err;
+//    console.log('文件内容截取成功');
+// });
+
+// 同步方法 文件内容截取
 var fs = require('fs');
-fs.truncate('./fileForRead.txt',6,function(err){
-   if (err) throw err;
-   console.log('文件内容截取成功');
-});
+try {
+ fs.truncateSync('./fileForRead.txt',3);
+ console.log('node.js 同步方法文件内容截取成功!')
+}catch (err) {
+    console.log('node.js 同步方法文件内容截取失败:'+err);
+}
